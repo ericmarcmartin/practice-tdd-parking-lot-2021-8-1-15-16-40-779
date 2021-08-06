68438d049c78ce09adc213ff0b2be54164811447
martier <martier@oocl.com>
2021-08-06 14:35:30 +0800

    [story 1][Eric] test: add should_return_parked_car_when_fetch_the_car_given_parking_lot_with_parked_car_and_ticket

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -19,4 +19,18 @@ class ParkingLotTest {
         assertNotNull(parkingTicket);
     }
 
+    @Test
+    public void should_return_parked_car_when_fetch_the_car_given_parking_lot_with_parked_car_and_ticket() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingTicket parkingTicket = new ParkingTicket();
+        Car car = new Car();
+
+        //when
+        Car actualCar = parkingLot.fetch(parkingTicket);
+
+        //then
+        assertEquals(actualCar, car);
+    }
+
 }
