138b8e539711ad03e1c17a41c57ba68a4ef7a31b
martier <martier@oocl.com>
2021-08-06 16:48:47 +0800

    [story 1][Eric] feat: add implementation to should_return_nothing_when_fetch_the_car_given_parking_lot_without_any_position_and_a_car

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class ParkingLot {
     private final Map<ParkingTicket, Car> parkedPosition = new HashMap<>();
     int capacity = 10;
+    int occupiedCapacity = 0;
 
     public ParkingLot(int capacity) {
         this.capacity = capacity;
@@ -16,12 +17,15 @@ public class ParkingLot {
 
 
     public ParkingTicket park(Car car) {
+        if (occupiedCapacity < capacity) {
             ParkingTicket parkingTicket = new ParkingTicket();
 
             parkedPosition.put(parkingTicket, car);
 
-            capacity++;
+            occupiedCapacity++;
             return parkingTicket;
+        }
+        return null;
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
