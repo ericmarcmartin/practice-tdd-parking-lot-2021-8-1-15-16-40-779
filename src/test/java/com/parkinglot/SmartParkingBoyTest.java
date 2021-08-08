02205992294eb2198bbb6a63a1bbaaa7bd04e719
martier <martier@oocl.com>
2021-08-08 17:59:23 +0800

    [Story5][Eric] test: add should_return_nothing_with_error_message_when_fetch_the_car_given_smart_parking_boy_with_2_parking_lots_and_a_used_ticket

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -193,4 +193,23 @@ class SmartParkingBoyTest {
         assertEquals("Unrecognized parking ticket.", exception.getMessage());
 
     }
+
+    @Test
+    public void should_return_nothing_with_error_message_when_fetch_the_car_given_smart_parking_boy_with_2_parking_lots_and_a_used_ticket() {
+        //given
+        List<ParkingLot> parkingLotList = Arrays.asList(
+                new ParkingLot(2),
+                new ParkingLot(2)
+        );
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLotList);
+        Car car = new Car();
+        ParkingTicket parkingTicket = smartParkingBoy.park(car);
+        smartParkingBoy.fetch(parkingTicket);
+
+        //when && then
+        Exception exception = assertThrows(UnrecognizedParkingTicketException.class, () ->
+                smartParkingBoy.fetch(parkingTicket));
+        assertEquals("Unrecognized parking ticket.", exception.getMessage());
+    }
+
 }
