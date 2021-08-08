d3bcc1b0ac661f0bb2c71e2dd4046994965e8a90
martier <martier@oocl.com>
2021-08-08 18:21:45 +0800

    [Story6][Eric] feat: add implementation to should_return_a_parked_car_when_fetch_the_car_given_parking_lot_with_a_parked_car_and_super_smart_parking_boy_and_a_ticket

--- src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SuperSmartParkingBoyTest {
     @Test
-    public void should_return_parking_ticket_when_park_the_car_given_parking_lot_and_smart_parking_boy_and_car() {
+    public void should_return_parking_ticket_when_park_the_car_given_parking_lot_and_super_smart_parking_boy_and_car() {
         //given
         Car car = new Car();
         ParkingLot parkingLot = new ParkingLot();
@@ -17,5 +17,20 @@ class SuperSmartParkingBoyTest {
 
         //then
         assertNotNull(parkingTicket);
+    }
+
+    @Test
+    public void should_return_a_parked_car_when_fetch_the_car_given_parking_lot_with_a_parked_car_and_super_smart_parking_boy_and_a_ticket() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLot);
+        Car car = new Car();
+        ParkingTicket parkingTicket = superSmartParkingBoy.park(car);
+
+        //when
+        Car actualCar = superSmartParkingBoy.fetch(parkingTicket);
+
+        //then
+        assertEquals(car, actualCar);
     }
 }
